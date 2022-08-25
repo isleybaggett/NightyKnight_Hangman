4d3bec672599949d8c69b1bc1fc8a53b8cb3dee8
isleybaggett <isleybaggett@gmail.com>
2022-08-24 23:26:11 -0500

    updated graphics

--- NightyKnight.py
+++ NightyKnight.py
@@ -6,38 +6,34 @@ import random
 #initiates pygame
 pygame.init()
 #sets window size
-window = pygame.display.set_mode([600, 900])
+window = pygame.display.set_mode([900, 900])
 
 #loads in all game images that i created
-gameBG = pygame.image.load('BG.png')
-knightHead = pygame.image.load('Head.png')
-knightBody = pygame.image.load('Body.png')
-knightRArm = pygame.image.load('RArm.png')
-knightLArm = pygame.image.load('LArm.png')
-knightRLeg = pygame.image.load('RLeg.png')
-knightLLeg = pygame.image.load('LLeg.png')
+gameBG = pygame.image.load('background.png')
+knightHead = pygame.image.load('headOnly.png')
+knightBody = pygame.image.load('headBody.png')
+knightRArm = pygame.image.load('oneArm.png')
+knightLArm = pygame.image.load('bothArms.png')
+knightRLeg = pygame.image.load('oneLeg.png')
+knightLLeg = pygame.image.load('fullBody.png')
 instructions = pygame.image.load('Instructions.png')
-home = pygame.image.load('Home.png')
-veggies = pygame.image.load('Veggies.png')
-fruits = pygame.image.load('Fruits.png')
-carMaker = pygame.image.load('CarMaker.png')
-colors = pygame.image.load('Color.png')
-antiVeggie = pygame.image.load('antiVeggie.png')
-antiFruit = pygame.image.load('antiFruit.png')
-antiCarMaker = pygame.image.load('antiCarMaker.png')
-antiColors = pygame.image.load('antiColor.png')
-winner = pygame.image.load('Winner.png')
-loser = pygame.image.load('Loser.png')
-restart = pygame.image.load('Restart.png')
-antiRestart = pygame.image.load('antiRestart.png')
+home = pygame.image.load('homeScreen.png')
+antiVeggie = pygame.image.load('vegTwo.png')
+antiFruit = pygame.image.load('fruitsTwo.png')
+antiCarMaker = pygame.image.load('carBrands.png')
+antiColors = pygame.image.load('colorTwo.png')
+playerScreen = pygame.image.load('playerScreen.png')
+winner = pygame.image.load('won.png')
+loser = pygame.image.load('loss.png')
+antiRestart = pygame.image.load('restartTwo.png')
 
 #creates a rectagle in pygame so it can detect when mouse is over location
 #used to swap image out with the anti image
-rectVeggie = pygame.Rect(180, 542, 240, 60) # x, y, width, height
-rectFruit = pygame.Rect(180, 438, 240, 60) 
-rectColors = pygame.Rect(180, 750, 240, 60) 
-rectCarMaker = pygame.Rect(180, 648, 240, 60) 
-rectRestart = pygame.Rect(230, 447, 140, 58)
+rectVeggie = pygame.Rect(127, 440, 300, 85) # x, y, width, height
+rectFruit = pygame.Rect(477, 310, 300, 85) 
+rectColors = pygame.Rect(477, 440, 300, 85) 
+rectCarMaker = pygame.Rect(127, 310, 300, 85) 
+rectRestart = pygame.Rect(300, 520, 300, 85)
 
 #global items that will be updated when the game resets
 missesLeft = 6 #misses allowed in game
@@ -52,7 +48,8 @@ displayList = []
 allCategoryList = ['MAZDA', 'HONDA', 'GMC', 'DODGE',
                    'BANANAS', 'STRAWBERRRY', 'APPLE', 'GRAPES', 
                    'CUCUMBER', 'TOMATOES', 'PEPPERS', 'LETTUCE',
-                   'PURPLE', 'GREEN', 'YELLOW', 'BLUE']
+                   'PURPLE', 'GREEN', 'YELLOW', 'BLUE', 'RED',
+                   'ORANGE', 'PINK', 'WHITE', 'BLACK']
 
 #used the random library to pick a word from the below categories 
 #function is used in the homeScreen function and passes back the secretWord to be used in gameLogic
@@ -60,7 +57,7 @@ def computer_Choice(category):
     carTypeList = ['MAZDA', 'HONDA', 'GMC', 'DODGE']
     fruitsList = ['BANANAS', 'STRAWBERRY', 'APPLE', 'GRAPES']
     veggiesList = ['CUCUMBER', 'TOMATOES', 'PEPPERS', 'LETTUCE']
-    colorsList = ['PURPLE', 'GREEN', 'YELLOW', 'BLUE']
+    colorsList = ['PURPLE', 'GREEN', 'YELLOW', 'BLUE', 'RED', 'ORANGE', 'PINK', 'WHITE', 'BLACK']
 #uses random based off the word provided by the button being clicked
     if category == 'car maker':
         secretWord = random.choice(carTypeList)
@@ -93,17 +90,19 @@ def gameScreen(misses):
     #the following text will always be displayed so it is added into this function
     #font being configured to what font style will be rendered
     font = pygame.font.Font('schluber.ttf', 50) #font name and size // i used dafont.com to download a free font (please see supporting files to credit the artist)
-    text = font.render("Your category is " + choice + '.', True, (10, 10, 10)) #renders my text 
-    textpos = text.get_rect(centerx = window.get_width() / 2, y = 30) #sets the position x position is centering on screen
-    text2 = font.render("Chances left = " + str(misses), True, (10, 10, 10))
-    text2pos = text.get_rect(centerx = window.get_width() / 2, y = 95)
+    text = font.render(choice, True, (236, 236, 236)) #renders my text 
+    textpos = text.get_rect(x = 165, y = 13) #sets the position x position is centering on screen
+    text2 = font.render(str(misses), True, (236, 236, 236))
+    text2pos = text.get_rect(x = 225, y = 70)
     window.blit(text, textpos) #blit the text just like a image
     window.blit(text2, text2pos)
 
     font2 = pygame.font.Font('schluber.ttf', 30) #create a second font size
     text3 = font2.render("Your word is:", True, (10, 10, 10))
     text3pos = text.get_rect(x = 50, y = 650)
-    window.blit(text3, text3pos)
+    #window.blit(text3, text3pos)
+
+    window.blit(playerScreen, (0, 0))
 
 #function runs to cover user input from the user
 #the hidden word and the users input are the values being used
@@ -137,16 +136,16 @@ def key_Pressed(word, letter):
     #below is used to print the guesses the user makes to the screen
     text = font2.render('Guesses:', True, (10, 10, 10))
     textPos = text.get_rect(centerx = window.get_width() / 2, y = 750)
-    window.blit(text, textPos)
-    a = 50 #sets the first x and y positions for printing the guesses
-    b = 800
+    #window.blit(text, textPos)
+    a = 60 #sets the first x and y positions for printing the guesses
+    b = 825
     for j in userChoices: #interates through user guesses to print to screen
-        guess = font2.render(j, True, (10, 10, 10))
+        guess = font2.render(j, True, (236, 236, 236))
         guessPos = guess.get_rect(x = a, y = b)
         window.blit(guess, guessPos)
         a = a + 50  #moves the next guess over
-        if a == 600: #if the guesses fill up the first row move down one and start again
-            b = 850  #i only allow six guesses so this is unused but available if you up the misses allowed
+        if a == 410: #if the guesses fill up the first row move down one and start again
+            b = 865  #i only allow six guesses so this is unused but available if you up the misses allowed
             a = 50
     #creates a list of letters and dashes to create the hidden word
     #if you wanted to use to word phrases elif would have to add a ' ' statement
@@ -156,13 +155,13 @@ def key_Pressed(word, letter):
         else:
             displayList.append('_')
     #interates the list created to blit it to the screen
-    i = 205
+    i = 430
     for j in displayList: 
-        dash = font.render(j, True, (10, 10, 10))
-        dashPos = dash.get_rect(x = i, y = 650)
+        dash = font.render(j, True, (236, 236, 236))
+        dashPos = dash.get_rect(x = i, y = 698)
         window.blit(dash, dashPos)
         i = i + 40
-    pygame.display.flip() #updates the screen with all blitted items
+    
  
 #checks to see if the mouse is in my rect statements 
 def is_over(rect, pos):
@@ -177,10 +176,7 @@ def homeScreen():
     pygame.mouse.set_visible(True) #makes the mouse visable not needed but wanted to add in
     pos = pygame.mouse.get_pos() #gets position of mouse
     window.blit(home, (0, 0)) #blits the starting images
-    window.blit(fruits, (0, 0))
-    window.blit(veggies, (0, 0))
-    window.blit(carMaker, (0, 0))
-    window.blit(colors, (0, 0)) 
+ 
 
     for event in pygame.mouse.get_pressed(): # Checks all events.
         if event == False: #if the mouse button is not clicked but hovering the following posistions it changes the image
@@ -217,10 +213,10 @@ def loserScreen(secretWord): #accepts the secret word
     pos = pygame.mouse.get_pos() #get the mouse position
     window.blit(loser, (0,0)) #add image
     font = pygame.font.Font('schluber.ttf', 30) #font style
-    text = font.render(secretWord, True, (10, 10, 10)) #adds the secret word so you know what word you missed
-    textpos = text.get_rect(x = 310, y = 390)
+    text = font.render(secretWord, True, (236, 236, 236)) #adds the secret word so you know what word you missed
+    textpos = text.get_rect(x = 459, y = 464)
     window.blit(text, textpos)
-    window.blit(restart, (0,0)) #blits the restart button
+
     pygame.event.clear() #clears out events 
     for event in pygame.mouse.get_pressed(): #waiting for mouse button to be clicked
         if is_over(rectRestart, pos):
@@ -228,12 +224,11 @@ def loserScreen(secretWord): #accepts the secret word
                 window.blit(antiRestart, (0, 0))
             elif event == True: #returns true to break the loop and restart the game if clicked
                 return True
-        pygame.display.flip() #updates the screen
+      
 
 
 def winnerScreen(): #shows that you won the game
     window.blit(winner, (0,0))
-    window.blit(restart, (0,0))
     pos = pygame.mouse.get_pos()
     pygame.event.clear() #clears out events
     for event in pygame.mouse.get_pressed(): #changes color of restart button
@@ -242,7 +237,8 @@ def winnerScreen(): #shows that you won the game
                 window.blit(antiRestart, (0, 0))
             elif event == True:
                 return True
-        pygame.display.flip() #updates screen
+     
+        
 
 #runs game processes
 def gameLogic(secretWord):
@@ -250,25 +246,25 @@ def gameLogic(secretWord):
     global missesLeft
     global userChoices
     window.blit(gameBG, (0, 0)) #starter screen 
-    window.blit(instructions, (0, 0)) #instructions telling you how to play
+    
     if pygame.font: #if font loads correctly then it does these things
         font = pygame.font.Font('schluber.ttf', 50) #same as in gameScreen just does it for the initial load
-        text = font.render("Your category is " + choice + '.', True, (10, 10, 10))
-        textpos = text.get_rect(centerx = window.get_width() / 2, y = 30)
-        text2 = font.render("Chances left = 6", True, (10, 10, 10))
-        text2pos = text.get_rect(centerx = window.get_width() / 2, y = 95)
+        text = font.render(choice, True, (236, 236, 236))
+        textpos = text.get_rect(x = 165, y = 13)
+        text2 = font.render("6", True, (236, 236, 236))
+        text2pos = text.get_rect(x = 225, y = 70)
         window.blit(text, textpos)
         window.blit(text2, text2pos)
         font2 = pygame.font.Font('schluber.ttf', 30) 
         text3 = font2.render("Your word is:", True, (10, 10, 10))
         text3pos = text.get_rect(x = 50, y = 650)
-        window.blit(text3, text3pos)
-
-    i = 205 #displays the user hidden word for the starting screen
+        #window.blit(text3, text3pos)
+        window.blit(playerScreen, (0, 0))
+    i = 430 #displays the user hidden word for the starting screen
     for j in range(0, len(secretWord)):
         displayList.append('_')
-        dash = font.render('_', True, (10, 10, 10))
-        dashPos = dash.get_rect(x = i, y = 650)
+        dash = font.render('_', True, (236, 236, 236))
+        dashPos = dash.get_rect(x = i, y = 698)
         window.blit(dash, dashPos)
         i = i + 40
     pygame.display.flip()
@@ -282,7 +278,10 @@ def gameLogic(secretWord):
                 sys.exit()
             elif '_' not in displayList: #if the list does not have the underscore that would mean you guessed the word and won
                 pygame.key.stop_text_input() #stops the keyboard from accepting input
-                winnerScreen() #displays the winner screen
+                window.fill((0, 0, 0))
+                key_Pressed(secretWord, keyCode)
+                winnerScreen()
+                pygame.display.flip()#displays the winner screen
                 while winnerScreen() == False: #if you havent clicked the mouse this is false and runs the winnerScreen loop
                     winnerScreen()
                 if winnerScreen() == True: #if you have clicked the restart this is true
@@ -292,7 +291,10 @@ def gameLogic(secretWord):
 
             elif missesLeft == 0: #if your misses left are zero then you lost the game and did not guess word
                 pygame.key.stop_text_input() #stops keyboard input
+                window.fill((0, 0, 0))
+                key_Pressed(secretWord, keyCode)
                 loserScreen(secretWord) #runs loser function
+                pygame.display.flip()
                 while loserScreen(secretWord) == False: #if not clicked false
                     loserScreen(secretWord)
                 if loserScreen(secretWord) == True: #if you hit restart
@@ -304,6 +306,7 @@ def gameLogic(secretWord):
                 keyCode = str(pygame.key.name(event.key))  #take the key hit and get the key name (found this in the doc to get the character) turn it into string
                 keyCode = keyCode.upper() #make it uppercase to compare to alphabet and words
                 key_Pressed(secretWord, keyCode) #pass that info to the key_Pressed function
+                pygame.display.flip()
                              
 #main loop
 run = True
